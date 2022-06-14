4994d573d91da917188dff5c6c37664dfc706630
Saif Uddin <saifctec@gmail.com>
2022-06-15 01:40:38 +0600

    NodeJs Project By Saif Date: 06/14/22 Time: 1.30pm

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 # Title:Raw NodeJs Project
 # Description: Uptime Monitoring App Using Raw Node Js
 # Author: Saif Uddin
-# Date:
+# Date:06-15-2022
 */
+
+// Dependencies
+const http = require('http');
+
+// app object - Module Scaffolding
+const app = {};
+
+// config
+app.config = {};
+
+// create server
+const server = http.createServer((req, res) => {});
